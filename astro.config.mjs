7fb9d900f6279d1630991de6be91da167fc225d2
Michał Bajerski <hello@michalbajerski.pl>
2026-06-21 10:40:18 +0200

    Fix: konfiguracja site i base pod GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Ważne: Zmień na swój dokładny login z GitHuba
-  site: 'https://michalbajerski.github.io',
+  site: 'https://mbajerski.github.io',
   
   // Ważne: Wpisz tu DOKŁADNĄ nazwę swojego repozytorium na GitHubie z ukośnikami!
   // Jeśli repozytorium nazywa się "michalbajerski.pl", wpisz '/michalbajerski.pl/'
